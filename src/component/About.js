dd0656c2113f7131a1f6646f7912895109b367d8
Abhinav Vishwakarma <abhinav.vishwakarma@gmail.com>
2023-06-22 17:43:58 +0530

    Added dark mode for About Page, The app is now complete.

--- src/component/About.js
+++ src/component/About.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 export default function About(props) {
-    // let sty = {
-    //     pass
-    // }
+    let tColor = '';
+    let bColor = '';
+    let sty = {}
+    if (props.mode === 'light'){
+        tColor = "black";
+        bColor = "#f8f9fa";
+    } else if (props.mode === 'dark') {
+        tColor = "white";
+        bColor = "#212529";
+    }
+    sty = {
+        color: tColor,
+        backgroundColor: bColor
+    }
     let txtColor = '';
     if (props.mode === 'light'){
         txtColor = "dark";
@@ -20,12 +31,12 @@ export default function About(props) {
     <div className="accordion" id="infoApp">
         <div className="accordion-item">
             <h2 className="accordion-header">
-                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={sty}>
                     What is Text Utils?
                 </button>
             </h2>
             <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#infoApp">
-                <div className="accordion-body">
+                <div className="accordion-body" style={sty}>
                     <p>TextUtils is an app which can be used for simple manipulation of text. It is a project from the ReactJs Course from Code With Harry</p>
                 </div>
             </div>

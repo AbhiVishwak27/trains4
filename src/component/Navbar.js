b55fcfee5416695623f990cf9ba0b6105406bfa7
AbhiVishwak27 <abhinav.vishwakarma2020@gmail.com>
2023-06-23 00:17:16 +0530

    improving react router

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -14,17 +14,17 @@ export default function Navbar(props) {
     <>
     <nav className={`navbar navbar-expand-lg bg-${props.mode}`}>
         <div className="container-fluid">
-            <Link className={`navbar-brand text-${txtColor}`} to="/">{props.title}</Link>
+            <Link className={`navbar-brand text-${txtColor}`} to="/TextUtils">{props.title}</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <Link className={`nav-link active text-${txtColor}`} aria-current="page" to="/">Home</Link>
+                        <Link className={`nav-link active text-${txtColor}`} aria-current="page" to="/TextUtils">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className={`nav-link active text-${txtColor}`} to="/about">{props.about}</Link>
+                        <Link className={`nav-link active text-${txtColor}`} to="/TextUtils/about">{props.about}</Link>
                     </li>
                 </ul>
                 <div className="form-check form-switch">

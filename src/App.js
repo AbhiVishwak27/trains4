bfd7e40d7bfb99860d541f547e890804e53f0592
Abhinav Vishwakarma <abhinav.vishwakarma@gmail.com>
2023-06-22 16:06:18 +0530

    setting  react router dom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import Navbar from './component/Navbar'
 import TextForm from './component/TextForm'
 import About from './component/About'
 import { useState } from 'react';
+import{
+  BrowserRouter as Router,
+  Link, 
+  Switch, 
+  Route
+} from 'react-router-dom'
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -18,11 +24,19 @@ function App() {
   }
   return (
     <>
-    <Navbar title="TextUtils" about="About TextUtils" mode={mode} handleMode={handleMode}/>
-    {/* <About/> */}
-    <div className="container">
-      <TextForm title="Enter The Text For Analyzing" mode={mode}/>
-    </div>
+    <Router>
+      <Navbar title="TextUtils" about="About TextUtils" mode={mode} handleMode={handleMode}/>
+      <Switch>
+        <Route path='/'>
+          <div className="container">
+            <TextForm title="Enter The Text For Analyzing" mode={mode}/>
+          </div>
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+      </Switch>
+    </Router>
     </>
   );
 }
